bc13d11e272a965624ca142760ba30913ee6af7f
demonicsweaters <demonicsweaters@gmail.com>
2017-05-11 10:59:24 -0400

    Update asampler.sh

--- asampler.sh
+++ asampler.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 ####################################################################################
-#					    asampler
-#				 a script for creating Amiga
-#				   samples on Raspberry Pi
+#					                                asampler
+#				                       a script for creating Amiga
+#				                          samples on Raspberry Pi
 #
-#   this script was designed to work with Amibian for capturing audio samples into a #   simple usb microphone input for the raspberry pi.
-#   It will first check your audio level then ask you three questions, what is the #   length of the recording in seconds?, 
+#   this script was designed to work with Amibian for capturing audio samples into a
+#   simple usb microphone input for the raspberry pi.
+#   It will first check your audio level then ask you three questions, what is the 
+#   length of the recording in seconds?, 
 #   what would you like to name the sample? And what sample rate?
 #
-#   Before you run it the first time make sure you put the path to your sample       #   directory below.
+#   Before you run it the first time make sure you put the path to your sample       
+#   directory below.
 #   you must have the program sox intalled before this will work.
 #
 #   by Demonic Sweaters licensed under GNU GPL License 2017 www.demonicsweaters.com
